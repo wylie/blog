4c715e634ed77e819f42a98571c00c4af896eac3
Wylie Fisher <me@wyliefisher.com>
2025-01-28 07:35:54 -0500

    still not working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import partytown from '@astrojs/partytown';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wylie.github.io',
-  integrations: [partytown()]
+  base: 'blog'
 });
